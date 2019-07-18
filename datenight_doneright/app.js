96403974257d53a6743b72fa4c2aa04fdab38010
Aegean <aegeanthompson@gmail.com>
2019-07-18 14:18:23 -0500

    modal 3 complete

--- datenight_doneright/app.js
+++ datenight_doneright/app.js
@@ -3,9 +3,14 @@ $( () => {
   const $openBtn = $('#openModal');
   const $modal = $('#modal');
   const $closeBtn = $('#close');
+
   const $openBtn2 = $('#buttonIn');
   const $modal2 = $('#modal2');
   const $closeBtn2 = $('#close2');
+
+  const $openBtn3 = $('#buttonMovie');
+  const $modal3 = $('#modal3');
+  const $closeBtn3 = $('#close3');
 
   const openModal = () => {
     $modal.css('display', 'block');
@@ -16,12 +21,21 @@ $( () => {
     $modal2.css('display', 'block');
   }
 
+  const openModal3 = () => {
+    $modal2.css('display', 'none');
+    $modal3.css('display', 'block');
+  }
+
   const closeModal = () => {
     $modal.css('display', 'none');
   }
 
   const closeModal2 = () => {
     $modal2.css('display', 'none');
+  }
+
+  const closeModal3 = () => {
+    $modal3.css('display', 'none');
   }
 
 
@@ -34,9 +48,14 @@ $( () => {
 
   $closeBtn2.on('click', closeModal2);
 
+  $openBtn3.on('click', openModal3);
 
-$.ajax({
+  $closeBtn3.on('click', closeModal3);
 
-            }
-        );
+
+
+// $.ajax({
+//
+//             }
+//         );
     });
